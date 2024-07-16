38391f98fd33213ee4a0865a8de3e3bd2cbde57a
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2024-07-15 23:47:39 -0300

    django

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import JsonResponse
-from genres.views import genre_create_list_view
+from genres.views import GenreCreatListView, genre_detail_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_create_list_view, name='genre-create-list'),
+    
+    path('genres/', GenreCreatListView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>', genre_detail_view, name='genre-detail-view'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,22 +1,41 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt # para nao dar erro ao fazer o post
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
+from rest_framework import generics
+from genres.serializers import GenreSerializer
+
+
+class GenreCreatListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    ...
 
 
 @csrf_exempt
-def genre_create_list_view(request):
-    if request.method == 'GET':
-        genres = Genre.objects.all()
-        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-        return JsonResponse(data, safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8')) #pego a requisição do body e docodifico 
-        # em utf-8 json.loads tranforma em dicionario
-        new_genre = Genre(name=data['name'])
-        new_genre.save() # salva no banco de dados
-        return JsonResponse(
-            {'id': new_genre.id, 'name': new_genre.name}, status=201,
-            ) # status=201 
-        #significa que foi criado com sucesso 
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
 
+    if request.method == 'GET':
+        data = {'id': genre.id, 'name': genre.name}
+        return JsonResponse(data)
+    
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        genre.name = data['name'] # o name vai receber um novo name
+        genre.save()
+        return JsonResponse(
+            {'id': genre.id, 'name': genre.name}
+        )
+
+    elif request.method == 'DELETE':
+        genre.delete()
+        return JsonResponse(
+            {'message': 'Gênero excluido com sucesso'},
+            status=204, # ao deletar um  objeto atraves de uma api rest o padrao é retornar um status204
+        )
+    
