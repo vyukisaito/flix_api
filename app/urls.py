fe57206b731734a2933461ba1539cc530298ee46
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2024-07-16 22:52:33 -0300

    update

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import GenreCreatListView, genre_detail_view
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('genres/', GenreCreatListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>', genre_detail_view, name='genre-detail-view'),
+    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+
+    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,41 +1,13 @@
-import json
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt # para nao dar erro ao fazer o post
-from django.shortcuts import get_object_or_404
-from genres.models import Genre
 from rest_framework import generics
+from genres.models import Genre
 from genres.serializers import GenreSerializer
 
 
-class GenreCreatListView(generics.ListCreateAPIView):
+class GenreCreateListView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    ...
-
-
-@csrf_exempt
-def genre_detail_view(request, pk):
-    genre = get_object_or_404(Genre, pk=pk)
-
-    if request.method == 'GET':
-        data = {'id': genre.id, 'name': genre.name}
-        return JsonResponse(data)
-    
-    elif request.method == 'PUT':
-        data = json.loads(request.body.decode('utf-8'))
-        genre.name = data['name'] # o name vai receber um novo name
-        genre.save()
-        return JsonResponse(
-            {'id': genre.id, 'name': genre.name}
-        )
-
-    elif request.method == 'DELETE':
-        genre.delete()
-        return JsonResponse(
-            {'message': 'Gênero excluido com sucesso'},
-            status=204, # ao deletar um  objeto atraves de uma api rest o padrao é retornar um status204
-        )
-    
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
