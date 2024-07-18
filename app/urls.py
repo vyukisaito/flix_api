367a8eab1d8d03c62e274b25d2e7f84d948b8794
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2024-07-18 16:07:42 -0300

    update

--- app/urls.py
+++ app/urls.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 
-    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
-
-    path('movie/', MovieCreateListView.as_view(), name='movie-create-list'),
-    path('movie/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+    path('api/v1/', include('genres.urls')), #versionamento 
+    path('api/v1/', include('actors.urls')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),
 ]
