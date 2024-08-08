a219c0379a5c4e87756c2e0bc6f2d072009c0d3d
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2024-08-07 22:08:41 -0300

    update

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalPermission
 from movies.models import Movie
@@ -15,3 +15,14 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalPermission)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
+
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalPermission)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        return response.Response(
+            data={'message:' 'Funcionou'},
+            status=status.HTTP_200_OK,
+        )
