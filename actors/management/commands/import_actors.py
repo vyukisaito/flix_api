848448de88f82b371d993f7c886dd7a98f304ae7
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2024-08-21 20:23:13 -0300

    django-command

--- actors/management/commands/import_actors.py
+++ actors/management/commands/import_actors.py
@@ -1,7 +1,34 @@
+import csv # biblioteca nativa do python
+from datetime import datetime
 from django.core.management.base import BaseCommand
+from actors.models import Actor # importa Actor para poder fazer o cadastro
 
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'file_name',
+            type=str,
+            help='Nome do arquivo com atores',
+        )
+
     def handle(self, *args, **options):
-        print('MEU PRIMEIRO COMANDO')
+        file_name = options['file_name']
+
+        with open(file_name, 'r', encoding='utf-8') as file: # abrindo o arquivo
+            reader = csv.DictReader(file) # ler o arquivo
+            for row in reader: # percorre linha a linha
+                name = row['name']
+                birthday = datetime.strptime(row['birthday'], '%Y-%m-%d').date() # le certo o arquivo quem tem data
+                nationality = row['nationality']
+
+                self.stdout.write(self.style.NOTICE(name))
+
+                Actor.objects.create(
+                    name=name,
+                    birthday=birthday,
+                    nationality=nationality,
+                )
+
+        self.stdout.write(self.style.SUCCESS('ATORES IMPORTADOS COM SUCESSO!')) # print tunado do djano command
